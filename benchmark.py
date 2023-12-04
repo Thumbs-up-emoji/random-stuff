6eaf182af71d7f03582c2dd86eb5efbe93e1df88
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-12-05 03:08:55 +0530

    try preprocessing yoga_pose_est

--- benchmark.py
+++ benchmark.py
@@ -1,4 +1,8 @@
-#L accuracy sadly
+# has some trouble with 7 vs 1, and sometimes doesn't actually copy to clipboard but good enough for now
+# need to try automatic input, hitting ctrl+v every time is good, but not enough
+# also can try pytesseract alternatives
+import cv2
+import numpy as np
 import pytesseract
 from PIL import ImageGrab
 import time
@@ -13,23 +17,32 @@ stop_program = False
 
 while True:
     # Capture the screen or a part of it
-    image = ImageGrab.grab(bbox=(100, 440, 800, 570))
+    image = ImageGrab.grab(bbox=(0, 450, 900, 580))
+    
+    # Convert the image to grayscale
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+
+    # Apply thresholding to binarize the image
+    _, image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
 
     # Read the text from the image
-    text = pytesseract.image_to_string(image)
+    text = pytesseract.image_to_string(image, config='--psm 6 outputbase digits')
 
-    #copy text to clipboard
+    # Copy text to clipboard
     pyperclip.copy(text)
 
     # Print the text
     print(text)
 
     # Start a separate loop to count down from 6 to 1
-    for i in range(3, 0, -1):
+    for i in range(2, 0, -1):
         # Print the remaining time before the next loop
         print(f"Time before next loop: {i} seconds")
 
-        # Check if 'q' and 'x' are pressed
+        # Check if 'esc' is pressed
         if keyboard.is_pressed('esc'):
             print("Stopping program.")
             stop_program = True
