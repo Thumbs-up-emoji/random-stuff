3d909b92bec2973dddd2f109f3add993c90d50b0
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-12-07 20:04:56 +0530

    perfect, but kinda slow

--- benchmark/reaction.py
+++ benchmark/reaction.py
@@ -3,17 +3,20 @@ import numpy as np
 import pyautogui
 
 # Define the pixel to monitor
-monitor = {"top": 200, "left": 200, "width": 1, "height": 1}
+monitor = {"top": 500, "left": 200, "width": 1, "height": 1}
 
 with mss.mss() as sct:
     while True:
         # Capture the pixel
         screenshot = sct.grab(monitor)
+        # print(screenshot) debug line
         # Convert the screenshot to a numpy array
         img = np.array(screenshot)
+        # print(img) debug line
 
         # Check the pixel
         if np.all(img[0, 0][:3] == [106, 219, 75]):
             # Click without moving the mouse
             print("Green!")
             pyautogui.click()
+            break
