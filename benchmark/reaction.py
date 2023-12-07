089adfb1bf29a011f99e3a8cdf1364b02e24f3db
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-12-07 20:44:27 +0530

    this is actually  perfect

--- benchmark/reaction.py
+++ benchmark/reaction.py
@@ -1,16 +1,25 @@
-from PIL import ImageGrab
+# tried automating it so it would click for the next attempt as well, idt that's worth it tho
+import mss
+import numpy as np
 import pyautogui
 
 # Define the pixel to monitor
-bbox = (200, 500, 201, 501)
+monitor = {"top": 500, "left": 200, "width": 1, "height": 1}
 
-while True:
-    # Capture the pixel
-    img = ImageGrab.grab(bbox=bbox)
-    # print(img.getpixel((0, 0))) debugging line
-    # Check the pixel
-    if img.getpixel((0, 0)) == (75, 219, 106): # colour in bgr
-        # Click without moving the mouse
-        print("Green!")
-        pyautogui.click()
-        break
+with mss.mss() as sct:
+    while True:
+        # Capture the pixel
+        screenshot = sct.grab(monitor)
+
+        # Convert the screenshot to a numpy array
+        img = np.array(screenshot)
+        
+        # print(img) debugging line
+        # print(screenshot) debugging line
+
+        # Check the pixel
+        if np.all(img[0, 0][:3] == [106, 219, 75]):
+            # Click without moving the mouse
+            print("Green!")
+            pyautogui.click()
+            break
