46b902cbe1b55c184c7e89716e36d0e96aa55700
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-12-11 18:55:23 +0530

    checking left

--- yt_scrape.py
+++ yt_scrape.py
@@ -13,14 +13,11 @@ choice = input("Enter the number of your choice: ")
 
 if choice == '1':
     pose_name = "tree"
-    pose_label = pose_name
 elif choice == '2':
     pose_name = "downward dog"
-    pose_label = "downdog"
 else:
     print("Invalid choice. Defaulting to 'downward dog'.")
     pose_name = "downward dog"
-    pose_label = "downdog"
 
 #define the search term
 search_term = "Yoga "+ pose_name
@@ -37,21 +34,21 @@ brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.ex
 for video in results:
     url = 'https://www.youtube.com' + video['url_suffix']
     subprocess.Popen([brave_path, '--incognito', url])
-    time.sleep(8)  # Wait for the browser window to become active
+    time.sleep(7)  # Wait for the browser window to become active
 
     # Click on the browser window to bring it into focus
-    pyautogui.click(x=400, y=500)
-
-    # Mute the video
-    pyautogui.press('m')
+    pyautogui.click(x=400, y=550) 
 
     # Press 'f' for fullscreen
     pyautogui.press('f')
     
-    # Wait for 1 seconds for fullscreen to load
+    # Mute the video
+    pyautogui.press('m')
+    
+    # Wait for 1 second for fullscreen to load
     time.sleep(1)
 
-    pyautogui.click(x=400, y=500) # play paused video
+    pyautogui.click(x=400, y=550) # play paused video
 
     for i in range(4):
         pyautogui.hotkey('shift','.') # 2x speed
@@ -61,17 +58,18 @@ for video in results:
         # Check if the escape key has been pressed
         if keyboard.is_pressed('esc'):
             break
+        
         # Capture the screen
         img = ImageGrab.grab()
-        if img is not None:
-            # Save the image as a JPG file
-            img.save(f'C:\\Users\\ASUS\\OneDrive - BITS Pilani K K Birla Goa Campus\\Desktop\\Stuff\\CS\\random-stuff\\yoga_pics\\{pose_name}{i}.jpg')
 
-            # Increment the image number
-            i += 1
+        # Save the image as a JPG file
+        img.save(f'C:\\Users\\ASUS\\OneDrive - BITS Pilani K K Birla Goa Campus\\Desktop\\Stuff\\CS\\random-stuff\\yoga_pics\\{pose_name}{i}.jpg')
 
-            # Wait for 0.5 seconds
-            time.sleep(60)
+        # Increment the image number
+        i += 1
+
+        # Wait for 0.5 seconds
+        time.sleep(0.5)
 
     # Close the current tab
     pyautogui.hotkey('ctrl', 'w')
