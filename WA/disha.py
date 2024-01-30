9870d8b16abda25e80f426ac89e815408bf166da
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2024-01-31 01:12:18 +0530

    removed fluff

--- WA/disha.py
+++ WA/disha.py
@@ -1,22 +1,11 @@
 # importing the module
 import pywhatkit
 import pyautogui
-import time
-
-# using Exception Handling to avoid 
-# unprecedented errors
-try:
 
 # sending message to receiver
 # using pywhatkit
-	pywhatkit.sendwhatmsg_instantly("+918008602198", "hello and namaste today", wait_time=85)
 
-	pyautogui.press("enter")	
+pywhatkit.sendwhatmsg_instantly("+918008602198", "hello and namaste today", wait_time=5)
+pyautogui.press("enter")	
+print("Successfully Sent!")
 
-	print("Successfully Sent!")
-
-except:
-
-# handling exception 
-# and printing error message
-	print("An Unexpected Error!")
