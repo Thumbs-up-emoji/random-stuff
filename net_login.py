30d059d3b98509b0b4756e1a529d1fb4bc52453f
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2024-08-02 12:06:47 +0530

    personal autocampnet

---
+++ net_login.py
@@ -0,0 +1,18 @@
+import webbrowser
+import pyautogui
+import time
+
+url = 'https://campnet.bits-goa.ac.in:8090/' 
+
+webbrowser.open(url)
+
+time.sleep(0.2)  
+
+pyautogui.click(x=888, y=473)  
+pyautogui.write('f20221197')
+
+pyautogui.click(x=882, y=543) 
+pyautogui.write('What_If317')
+
+pyautogui.press('enter')
+pyautogui.hotkey('ctrl','w')  
--- to_do/yoga_sort_ML.py
+++
@@ -1,2 +0,0 @@
-#sort yoga poses into one of 2 types using ML methods, specifically neural networks
-#can try decision tree and logistic regression too
