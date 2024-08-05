cc0b517d911c9a9d3786b473019bc26df244f355
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2024-08-05 16:10:40 +0530

    Update net_login.py

--- net_login.py
+++ net_login.py
@@ -6,7 +6,7 @@ url = 'https://campnet.bits-goa.ac.in:8090/'
 
 webbrowser.open(url)
 
-time.sleep(0.3)  
+time.sleep(0.4)  
 
 pyautogui.click(x=888, y=473)  
 pyautogui.write('f20221197')
@@ -15,4 +15,4 @@ pyautogui.click(x=882, y=543)
 pyautogui.write('What_If317')
 
 pyautogui.press('enter')
-#pyautogui.hotkey('ctrl','w')  
+pyautogui.hotkey('ctrl','w')  
