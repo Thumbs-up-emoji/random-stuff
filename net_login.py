bb9d4bb9477eee2f18e95b04dc8917abcca2222c
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2024-08-21 20:13:46 +0530

    credentials changed

--- net_login.py
+++ net_login.py
@@ -9,10 +9,10 @@ webbrowser.open(url)
 time.sleep(0.4)  
 
 pyautogui.click(x=888, y=473)  
-pyautogui.write('f20221197')
+pyautogui.write('f20201384')
 
 pyautogui.click(x=882, y=543) 
-pyautogui.write('What_If317')
+pyautogui.write('qwerty123')
 
 pyautogui.press('enter')
 pyautogui.hotkey('ctrl','w')  
