d11a8fe4ec6c23123bf447fce5eeaa9489943845
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2024-09-29 11:11:07 +0530

    firefox

--- net_login.py
+++ net_login.py
@@ -8,10 +8,10 @@ webbrowser.open(url)
 
 time.sleep(1)  
 
-pyautogui.click(x=888, y=473)  
+pyautogui.click(x=969, y=496)  
 pyautogui.write('f20201384')
 
-pyautogui.click(x=882, y=543) 
+pyautogui.click(x=963, y=568) 
 pyautogui.write('qwerty123')
 
 pyautogui.press('enter')
