39bf8fbf47651dc1122efc4aeb13cf6c9c594765
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2024-10-03 03:46:32 +0530

    dark day

--- net_login.py
+++ net_login.py
@@ -8,11 +8,11 @@ webbrowser.open(url)
 
 time.sleep(1)  
 
-pyautogui.click(x=969, y=496)  
-pyautogui.write('f20201384')
+pyautogui.click(x=1200, y=496)  
+pyautogui.write('f20221197')
 
-pyautogui.click(x=963, y=568) 
-pyautogui.write('qwerty123')
+pyautogui.click(x=1200, y=568) 
+pyautogui.write('What_If317')
 
 pyautogui.press('enter')
 pyautogui.hotkey('ctrl','w')  
