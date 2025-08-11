dc514c10c42b16b95211b0a777d978c70472c20d
Thumbs-up-emoji <f20221197@goa.bits-pilani.ac.in>
2025-08-11 20:18:55 +0530

    nlin

--- net_login.py
+++ net_login.py
@@ -1,18 +1,35 @@
 import webbrowser
 import pyautogui
 import time
+import csv
+import sys
+import random
+
+def get_login_details(line_num):
+    with open('/home/vk/CS/random-stuff/login_details.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for idx, row in enumerate(reader, start=1):
+            if idx == line_num:
+                return row[0], row[1]
+    raise ValueError("Line number out of range in login_details.csv")
+
+if len(sys.argv) < 2:
+    line_number = random.randint(1, 9)
+    print(f"No line number provided. Using random line: {line_number}")
+else:
+    line_number = int(sys.argv[1])
+
+username, password = get_login_details(line_number)
 
 url = 'https://campnet.bits-goa.ac.in:8090/' 
-
 webbrowser.open(url)
-
 time.sleep(1)  
 
-pyautogui.click(x=900, y=450)  
-pyautogui.write('f20221197')
+pyautogui.click(x=1037, y=367)  
+pyautogui.write(username)
 
-pyautogui.click(x=900, y=520) 
-pyautogui.write('What_If317')
+pyautogui.click(x=1044, y=432) 
+pyautogui.write(password)
 
 pyautogui.press('enter')
 pyautogui.hotkey('ctrl','w')  
